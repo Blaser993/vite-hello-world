89c38f6f55fe32a63af9874965362ae0c670a68f
Blaser993 <corradodepinto.design@gmail.com>
2023-02-28 17:28:45 +0100

    FEAT AppTitle component created

--- .vue-vite/src/App.vue
+++ .vue-vite/src/App.vue
@@ -1,15 +1,42 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+
+import AppTitle from "./components/AppTitle.vue"
+
+
+export default{
+
+  components: {
+    AppTitle
+  },
+
+  data (){
+    return {
+      message: "ciao vite",
+    }
+  },
+
+  methods:{
+    method(){
+      console.log(method)
+    }
+  }
+}
 </script>
 
 <template>
+
+
+  <AppTitle></AppTitle>
+
   <div>
+    <h1>{{message}}</h1>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
+
   </div>
   <HelloWorld msg="Vite + Vue" />
 </template>
